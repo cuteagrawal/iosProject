9eba65676e2d526abcc0345129f1386ef1560dae
cute agrawal <63496849+cuteagrawal@users.noreply.github.com>
2024-04-01 19:07:11 -0400

    Previous tweet View controller added
    Added a new view controller for previous tweets. Added basic UI and connected all the view controllers

---
+++ FinalDestination/FinalDestination/OldTweetsViewController.swift
@@ -0,0 +1,32 @@
+//
+//  OldTweetsViewController.swift
+//  FinalDestination
+//
+//  Created by Cute Agrawal on 2024-04-01.
+//
+
+import UIKit
+
+class OldTweetsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func unwindToOldTweetsView(sender : UIStoryboardSegue)
+    {
+    // typically empty unless special code needed
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
