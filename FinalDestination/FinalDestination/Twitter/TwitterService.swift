cff7d50de4a02c3b8d2d88dd43899b756bbf4489
cute agrawal <63496849+cuteagrawal@users.noreply.github.com>
2024-04-14 15:11:10 -0400

    added comments on my work

--- FinalDestination/FinalDestination/Twitter/TwitterService.swift
+++ FinalDestination/FinalDestination/Twitter/TwitterService.swift
@@ -3,21 +3,30 @@ import OAuthSwift
 
 // package: https://github.com/OAuthSwift/OAuthSwift.git
 
+/*
+ * This class works with X API (Twitter)
+ * I am using a package to get authentication from Twitter. New API does not allow
+ * direct data to be sent without authentication
+ */
 
 class TwitterService {
     
+    // Creates only one version of the class
     static let shared = TwitterService()
     
+    // This is the URL where the API sends the data
     let baseURL = "https://api.twitter.com/2/tweets"
     
     private init() {}
     
+    // This generate the tweet usig API Key, secret key, access token and secret token
     func generateTweet(for tweet: String, completion: @escaping (Bool) -> Void) {
         let apiKey = "cmydQISfwxrppKdrBRApmeRNL"
         let apiSecretKey = "OOfxhKTmRHDTyPea9iQEnLauVpUccrnwsSN7MT8lp1qCWx2fwo"
         let accessToken = "2989656215-oYmiEgiK6pBjc4VPExra0GKgVmk2YgAlS94Brrx"
         let accessSecretToken = "w5WlquHrLNclLzkNlvJ1kS3joWfmVrL15k8H5kL09vIXO"
         
+        // using authentication token
         let oauthClient = OAuthSwiftClient(consumerKey: apiKey, consumerSecret: apiSecretKey, oauthToken: accessToken, oauthTokenSecret: accessSecretToken, version: .oauth1)
         
         /*
@@ -33,6 +42,7 @@ class TwitterService {
             return
         }
         
+        // post request with jsonised data (tweet)
         oauthClient.post(
             baseURL,
             parameters: parameters,
@@ -52,6 +62,7 @@ class TwitterService {
 }
 
 
+// Used POSTMAN to check the API
 
 /*
  information from POSTMAN
