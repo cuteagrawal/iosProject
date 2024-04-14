61da6b3174949230060d962990441fd2d9381810
Jay Patel <81171374+Jaypatel2510@users.noreply.github.com>
2024-04-14 18:52:09 +0000

    Comments is added in all ViewControllers

--- FinalDestination/FinalDestination/Budget/OldBudgetViewController.swift
+++ FinalDestination/FinalDestination/Budget/OldBudgetViewController.swift
@@ -7,8 +7,12 @@
 
 import UIKit
 
+/**
+ * ViewController which will show the previoud Budgets in table form
+ */
 class OldBudgetViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
+    // AppDelegate instance to get the viewBudget property
     let mainDelegate = UIApplication.shared.delegate as! AppDelegate
     
     @IBOutlet var budgetTable: UITableView!
@@ -16,26 +20,32 @@ class OldBudgetViewController: UIViewController, UITableViewDataSource, UITableV
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Calling read database methond in AppDelegate and assigning null to viewBudget
         mainDelegate.readDataFromDatabase()
         mainDelegate.viewBudget = nil
     }
     
-    
+    // Method which will return the row numbers for table
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return mainDelegate.budgetArr.count
     }
     
+    // Method to set height of table
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 80
     }
     
+    // Method to formate cells
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let tableCell : BudgetTableViewCell = tableView.dequeueReusableCell(withIdentifier: "OldBudgetCell") as? BudgetTableViewCell ?? BudgetTableViewCell(style: .default, reuseIdentifier: "OldBudgetCell")
         
         let rowNum = indexPath.row
         
+        // Assigning Destiantion value to Budget Destination lable
         tableCell.budgetDestination.text = mainDelegate.budgetArr[rowNum].destination
+        
+        // Assigning Currency value to Budget Currency lable
         tableCell.budgetCurrency.text = mainDelegate.budgetArr[rowNum].currency
         
         tableCell.accessoryType = .disclosureIndicator
@@ -43,10 +53,12 @@ class OldBudgetViewController: UIViewController, UITableViewDataSource, UITableV
         return tableCell
     }
     
+    // Method to allowing to edit each rows in table
     func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         return true
     }
     
+    // Method will invoke when rows of table is clicked
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
         mainDelegate.viewBudget = mainDelegate.budgetArr[indexPath.row]
