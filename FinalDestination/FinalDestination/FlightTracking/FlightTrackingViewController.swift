01747c351259263fe854d13f4a04493b1a9bbf3b
Feby Jomy <63770114+febyjomy@users.noreply.github.com>
2024-04-14 14:31:22 +0000

    Some changes made,
    This commit is to save the code, My XCode is having issues.

--- FinalDestination/FinalDestination/FlightTracking/FlightTrackingViewController.swift
+++ FinalDestination/FinalDestination/FlightTracking/FlightTrackingViewController.swift
@@ -8,17 +8,41 @@
 import UIKit
 
 class FlightTrackingViewController: UIViewController {
+    // YYZ, YUL, LAX, etc..
     @IBOutlet var departureAirportCodeLbl: UILabel!
     @IBOutlet var arrivalAirportCodeLbl: UILabel!
+    
+    // Search Box
     @IBOutlet var flightSearchTxtFld: UITextField!
-
+    
+    // Timings
+    @IBOutlet var departureTimeLbl: UILabel!
+    @IBOutlet var arriveTimeLbl: UILabel!
+    
+    // Sched, live, landed etc..
+    @IBOutlet var flightStatusLbl: UILabel!
+    
+    //Toronto to Montreal, Abu Dhabi to Los Angeles etc...
+    @IBOutlet var flightPlanCitiesLbl: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
+    // Search function
+    @IBAction func searchFlight(sender: UIButton){
+        
+    }
+    
     // Unwins segue
     @IBAction func unwindToFlightTrackingVC (sender: UIStoryboardSegue) { }
-
+    
+    // Add to calendar action
+    @IBAction func addToCalendar(sender: UIButton){
+        
+    }
+    
 }
